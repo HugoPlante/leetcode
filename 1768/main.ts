77ea7d9463c26867d3f47688cc68a40272d2a80a
Hugo Plante <hugo.plante.2@ulaval.ca>
2024-01-14 13:29:06 -0500

    1768 remove other solutions

--- 1768/main.ts
+++ 1768/main.ts
@@ -1,25 +1,4 @@
-function mergeAlternately(word1: string, word2: string): string {
-  let merged = Array(word1.length + word2.length).fill("");
-
-  let w1Pointer = 0;
-  let w2Pointer = 0;
-
-  for (let i = 0; i < merged.length; i++) {
-    if (w1Pointer < word1.length) {
-      merged[i] = word1[w1Pointer];
-      w1Pointer++;
-    }
-
-    if (w2Pointer < word2.length) {
-      merged[i] += word2[w2Pointer];
-      w2Pointer++;
-    }
-  }
-
-  return merged.join("");
-}
-
-function mergeAlternately2(word1, word2) {
+function mergeAlternately(word1, word2) {
   let merged = "";
   let w1Pointer = 0;
   let w2Pointer = 0;
@@ -31,24 +10,6 @@ function mergeAlternately2(word1, word2) {
     if (w2Pointer < word2.length) {
       merged += word2[w2Pointer++];
     }
-  }
-
-  return merged;
-}
-
-function mergeAlternately3(word1, word2) {
-  let merged = "";
-  let i = 0;
-
-  while (i < word1.length && i < word2.length) {
-    merged += word1[i] + word2[i];
-    i++;
-  }
-
-  if (i < word1.length) {
-    merged += word1.slice(i);
-  } else if (i < word2.length) {
-    merged += word2.slice(i);
   }
 
   return merged;
