37f749f8f2d2d8b57abcf4499d05fe829fc9dd57
Hugo Plante <hugo.plante.2@ulaval.ca>
2024-01-18 14:45:13 -0500

    daily challenge

---
+++ 70/main.ts
@@ -0,0 +1,14 @@
+function climbStairs(n: number): number {
+    if (n <= 2) return n;
+
+    let prev = 1, B_prev = 2;
+    let current = 0;
+
+    for (let loop = 1; loop < n - 1; loop++) {
+        current = prev + B_prev; // Calculate current steps
+        prev = B_prev;
+        B_prev = current;
+    }
+
+    return current;
+};
